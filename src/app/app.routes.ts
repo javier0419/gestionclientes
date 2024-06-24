709f86bb1d35c7e16b3efb6a044f29d2937222e1
luis javier visitor noza <visitornozaluisjavier@gmail.com>
2024-06-24 18:46:41 -0400

    Avance

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { AboutComponent } from './pages/about/about.component';
+    
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'inicio', component: InicioComponent},
+    {path: 'scercade', component: AboutComponent},
+    {path: '**', component: InicioComponent},
+];
+@NgModule({
+    imports: [RouterModule.forRoot(routes,{useHash: true})],
+    exports: [RouterModule]
+})
+export class AppRoutingModule{}
